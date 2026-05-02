45fadc73020534629212a2519e076d8040220505
Archit <archit@192.168.1.8>
2026-05-02 14:13:10 +0530

    beta 0.20

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/features', function () {
+    return view('features');
+});
+
+Route::get('/pricing', function () {
+    return view('pricing');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
 Route::get('/pos/{any?}', function () {
    return response()->file(public_path('pos/index.html'));
 })->where('any', '.*');
