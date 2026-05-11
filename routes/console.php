4ee2dc3a96215a45275613641447c2256d407750
Archit <archit@192.168.1.5>
2026-05-11 15:04:07 +0530

    beta 0.28

--- routes/console.php
+++ routes/console.php
@@ -8,7 +8,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('reports:generate')
+Schedule::command('app:generate-reports')
     ->hourly()
     ->withoutOverlapping()
     ->runInBackground();
