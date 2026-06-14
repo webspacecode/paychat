10bf30cb35b225c2f9ae7067b39012f6f5251e2b
Archit <archit@192.168.1.10>
2026-06-14 12:34:30 +0530

    beta 0.117

---
+++ public/pos/assets/locationService-BypfKS99.js
@@ -0,0 +1,1 @@
+import{s as o}from"./index-qYCsh7tZ.js";const a={list(t={}){return localStorage.getItem("tenant_slug"),o.get("/locations",{params:t})}};export{a as l};
--- public/pos/assets/locationService-DJfag-nL.js
+++
@@ -1,1 +0,0 @@
-import{s as o}from"./index-Cm-86Cal.js";const a={list(t={}){return localStorage.getItem("tenant_slug"),o.get("/locations",{params:t})}};export{a as l};
