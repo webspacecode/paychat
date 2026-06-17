f3f9f916cf391a638f9db46054a9d158e3d5e5cf
Archit <archit@ip-192-168-1-11.ec2.internal>
2026-06-17 18:48:14 +0530

    beta 0.120

---
+++ public/pos/assets/locationService-2Poz2gBF.js
@@ -0,0 +1,1 @@
+import{b as o}from"./index-D-u9MWYA.js";const a={list(t={}){return localStorage.getItem("tenant_slug"),o.get("/locations",{params:t})}};export{a as l};
--- public/pos/assets/paymentService-BNyTZl3F.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index-C8uqlr2x.js";const a={getMethods(){return localStorage.getItem("tenant_slug"),t.get("/payments/methods")}};export{a as p};
