9c12fc7052c4c261e83b5f6b3cf0d49bd51e466f
Archit <archit@Archits-iMac.local>
2026-07-02 19:04:29 +0530

    beta 0.161

---
+++ public/pos/assets/locationService-BMuU--7P.js
@@ -0,0 +1,1 @@
+import{b as o}from"./index-U8mh1bMj.js";const a={list(t={}){return localStorage.getItem("tenant_slug"),o.get("/locations",{params:t})}};export{a as l};
---
+++ public/pos/assets/paymentService-cHw-HbPA.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index-U8mh1bMj.js";const a={getMethods(){return localStorage.getItem("tenant_slug"),t.get("/payments/methods")}};export{a as p};
