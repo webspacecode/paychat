27efbfe5f3ac9490a714212b521fe65f977baf87
Archit <archit@192.168.1.8>
2026-07-02 13:31:26 +0530

    beta 0.156

---
+++ public/pos/assets/locationService-zbd3zdkz.js
@@ -0,0 +1,1 @@
+import{b as o}from"./index-CQ5ofadG.js";const a={list(t={}){return localStorage.getItem("tenant_slug"),o.get("/locations",{params:t})}};export{a as l};
---
+++ public/pos/assets/paymentService-BmWJcglf.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index-CQ5ofadG.js";const a={getMethods(){return localStorage.getItem("tenant_slug"),t.get("/payments/methods")}};export{a as p};
