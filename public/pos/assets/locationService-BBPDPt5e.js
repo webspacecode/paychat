3b12d5693df87d4b1444a53c45803ae5bea285c7
Archit <archit@ip-192-168-1-11.ec2.internal>
2026-07-07 22:15:10 +0530

    beta 0.155

---
+++ public/pos/assets/locationService-BBPDPt5e.js
@@ -0,0 +1,1 @@
+import{b as o}from"./index-D9PVPxpt.js";const a={list(t={}){return localStorage.getItem("tenant_slug"),o.get("/locations",{params:t})}};export{a as l};
---
+++ public/pos/assets/paymentService-BM2yTgud.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index-D9PVPxpt.js";const a={getMethods(){return localStorage.getItem("tenant_slug"),t.get("/payments/methods")}};export{a as p};
